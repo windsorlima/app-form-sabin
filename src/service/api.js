2939f6b8f5bcf4f0307dfa1cf42486a9ce206294
Windsor Lima <windsorlimad@gmail.com>
2023-06-13 15:30:46 -0300

    fix: change api and store

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://portal.albertsabin.com.br:8095",
-  baseURL: "http://localhost:3500",
+  baseURL: "https://portal.albertsabin.com.br:8095",
+  // baseURL: "http://localhost:3500",
 });
--- src/store/app.js
+++ src/store/app.js
@@ -1,5 +1,5 @@
 export const defaultInitialValues = {
   studentName: "",
-  students: [{ name: "Windsor Lima", grade: "805T4", id: "201309355" }],
-  // students: [],
+  // students: [{ name: "Windsor Lima", grade: "805T4", id: "201309355" }],
+  students: [],
 };
